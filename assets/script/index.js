adf3749932afad6c9cabb2ec7008c5fca01acf23
raman tehlan <ramantehlan@gmail.com>
2019-02-10 22:16:58 +0530

    Menu functions added

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,12 @@
 "use strict";
 
-var menu = document.getElementById("menuIcon");
-menu.addEventListener('click', function() {
-    alert('Hello world');
+var menuIcon = document.getElementById("menuIcon");
+var menuList = document.getElementById("menuList");
+
+menuIcon.addEventListener('click', function() {
+if (menuList.className === "menu-list") {
+  menuList.className += " menu-list-responsive";
+} else {
+  menuList.className = "menu-list";
+}
 }, false);
