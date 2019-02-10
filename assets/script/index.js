2a0f06926e214ae045ea7044e26e08e68ef2cda0
raman tehlan <ramantehlan@gmail.com>
2019-02-11 00:21:47 +0530

    Menu Updated

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,12 +1,17 @@
 "use strict";
 
+var menu = document.getElementById("menu");
 var menuIcon = document.getElementById("menuIcon");
 var menuList = document.getElementById("menuList");
 
 menuIcon.addEventListener('click', function() {
 if (menuList.className === "menu-list") {
   menuList.className += " menu-list-responsive";
+  menuIcon.className += " menu-icon-clicked";
+  menu.className += " menu-down";
 } else {
   menuList.className = "menu-list";
+  menuIcon.className = "fas fa-bars menu-icon";
+  menu.className = "menu";
 }
 }, false);
