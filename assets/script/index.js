b021a36ed77bf5de4db3788b751369008d7e1f34
raman tehlan <ramantehlan@gmail.com>
2019-02-11 14:50:26 +0530

    Vue data added from data.json

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,34 +1,46 @@
 "use strict";
 
-var menu = document.getElementById("menu");
-var menuIcon = document.getElementById("menuIcon");
-var menuList = document.getElementById("menuList");
-
-menuIcon.addEventListener('click', function() {
-if (menuList.className === "menu-list") {
-  menuList.className += " menu-list-responsive";
-  menuIcon.className += " menu-icon-clicked";
-  menu.className += " menu-down";
-} else {
-  menuList.className = "menu-list";
-  menuIcon.className = "fas fa-bars menu-icon";
-  menu.className = "menu";
-}
-}, false);
-
-fetch('data.json')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                console.log(data);
-            }).catch(function (error) {
-                console.log(error);
-        });
-
 var app = new Vue({
   el: '#view',
   data: {
-
-    }
+    render: false
+  }
 })
+
+fetch('data.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Json Loaded");
+    app.data = data;
+    app.render = true;
+    menuToggle();
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+function menuToggle() {
+  var menu = _("menu");
+  var menuIcon = _("menuIcon");
+  var menuList = _("menuList");
+  var menuTitle = _('menuTitle');
+
+  menuIcon.addEventListener('click', function() {
+    if (menuList.className === "menu-list") {
+      menuList.className += " menu-list-responsive";
+      menuIcon.className += " menu-icon-clicked";
+      menu.className += " menu-down";
+      menuTitle.style = "display:none";
+    } else {
+      menuList.className = "menu-list";
+      menuIcon.className = "fas fa-bars menu-icon";
+      menu.className = "menu";
+      menuTitle.style = "display:block";
+    }
+  }, false);
+}
+
+function _(div){
+  return document.getElementById(div);
+}
